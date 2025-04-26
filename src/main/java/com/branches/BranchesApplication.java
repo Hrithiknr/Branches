0fc8886d4fec9c00e40ddf927ec05db541ec80d2
Hrithik <hrithikhruth15@gmail.com>
2025-04-26 10:27:26 +0530

    defectfix-3456-login-error

--- src/main/java/com/branches/BranchesApplication.java
+++ src/main/java/com/branches/BranchesApplication.java
@@ -9,6 +9,8 @@ public class BranchesApplication {
 	public static void main(String[] args) {
 		int x=100;
 		int y=2000;
+		int a=x*y;
+
 		int z = x+y;
 		SpringApplication.run(BranchesApplication.class, args);
 	}
